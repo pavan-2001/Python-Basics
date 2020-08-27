3d705146201c95d0b032106a89d2fec904363160
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-08-27 22:45:54 +0530

    Update Democode.py

--- Collections/Dictionary/Democode.py
+++ Collections/Dictionary/Democode.py
@@ -1,9 +1,13 @@
 states={'State_1':'Hydrabad','State_2':'Telangana'}
+print('Before ading new key-value pairs : ',states)
+
 states['State_3']='Maharashtra'
 states['State_4']='Rajasthan'
-print(states)
-print(states['State_1'])
-print(states['State_2'])
-print(states['State_3'])
-print(states['State_4'])
-print(len(states))
+print()
+print('After adding new key-value pairs : ',states)
+print()
+print('All key-value pairs ')
+for item in states.items():
+    print(item)
+print()
+print('The size of the Dictionary is : ',len(states))
