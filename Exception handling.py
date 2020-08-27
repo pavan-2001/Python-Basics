4a4a07e1810052d47f59d3819caa9efcb426bf88
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-08-27 20:57:09 +0530

    Create Exception handling.py

---
+++ Exception handling.py
@@ -0,0 +1,10 @@
+x=5
+y=0
+try:
+    print(x/y)
+except ZeroDivisionError as e:
+    print('we cannot divide a number by zero')
+else:
+    print('something went wrong')
+finally:
+    print('pavan')
