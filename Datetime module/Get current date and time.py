631f9da5de136147ee5aaeaa1f76f779386b61df
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-08-28 11:32:08 +0530

    Update and rename Datetime module/Taking date as input/Democode.py to Datetime module/Get current date and time.py

---
+++ Datetime module/Get current date and time.py
@@ -0,0 +1,12 @@
+from dateime import datetime 
+datetime_object=datetime.now()
+print(datetime_object)
+
+
+
+"""
+Output 
+
+2020-08-28 11:29:59.162327
+
+"""
--- Datetime module/Taking date as input/Democode.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime,timedelta
-birthday=input('enter your date of birth(dd/mm/yyyy): ')
-birthday_date=datetime.strptime(birthday,'%d/%m/%Y')
-print(str(birthday_date))
