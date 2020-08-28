53483afc84a4cecd14f636ffce78c9c04fe7eaf1
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-08-28 12:06:09 +0530

    Update and rename Get current date.py to Get current date in different formats.py

---
+++ Datetime module/Get current date in different formats.py
@@ -0,0 +1,19 @@
+from datetime import date 
+today=date.today()
+
+print('Format (dd/mm/yy) : ',today.strftime('%d/%m/%y'))
+
+print('Format (mm/dd/yyyy) : ',today.strftime('%m/%d/%Y'))
+
+print('Format (Textual month/dd/yyyy) : ',today.strftime('%B-%d-%Y'))
+
+print('Format (Month abrev.-dd-yyyy) : ',today.strftime('%b-%d-%Y'))
+"""
+Output
+
+Format (dd/mm/yy) :  28/08/20
+Format (mm/dd/yyyy) :  08/28/2020
+Format (Textual month/dd/yyyy) :  August-28-2020
+Format (Month abrev. ,dd,yyyy) :  Aug-28-2020
+
+"""
--- Datetime module/Get current date.py
+++
@@ -1,6 +0,0 @@
-from datetime import date
-todays_date=date.today()
-print(todays_date)
-"""
-Output-   2020-08-28
-"""
