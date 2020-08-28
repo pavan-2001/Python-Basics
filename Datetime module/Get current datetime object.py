ff3e51a4dae840d02a6e65419ce6a8fda2886030
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-08-28 13:12:09 +0530

    Create Get current datetime object.py

---
+++ Datetime module/Get current datetime object.py
@@ -0,0 +1,22 @@
+from datetime import datetime,time
+now=datetime.now()
+print(f"Hours : {now.hour}\n")
+print(f"Minutes : {now.minute}\n")
+print(f"Seconds : {now.second}\n")
+print(f"Microseconds : {now.microsecond}\n")
+print(f"Current month : {now.month}\n")
+print(f"Current year : {now.year}\n")
+"""
+Output-
+Hours : 13
+
+Minutes : 11
+
+Seconds : 29
+
+Microseconds : 248210
+
+Current month : 8
+
+Current year : 2020
+"""
