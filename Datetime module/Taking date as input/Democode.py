09c9b303814567dd8f59e227cc28451239102b7e
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-08-28 11:10:06 +0530

    Update and rename Datetime module/Democode.py to Datetime module/Taking date as input/Democode.py

--- Datetime module/Taking date as input/Democode.py
+++ Datetime module/Taking date as input/Democode.py
@@ -1,4 +1,4 @@
 from datetime import datetime,timedelta
 birthday=input('enter your date of birth(dd/mm/yyyy): ')
 birthday_date=datetime.strptime(birthday,'%d/%m/%Y')
-print(str(birthday_date-timedelta(weeks=2)))
+print(str(birthday_date))
