47edca858ec494e8f99dbaf3f6fdd1a9d8be5f76
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-08-28 12:50:08 +0530

    Create Get month,day,year.py

---
+++ Datetime module/Get month,day,year.py
@@ -0,0 +1,14 @@
+from datetime import date 
+today=date.today()
+print(f"Today's date is : {today.day}\n")
+print(f"Current month is : {today.month}\n")
+print(f"Current year is : {today.year}\n")
+
+"""
+Output-
+Today's date is : 28
+
+Current month is : 8
+
+Current year is : 2020
+"""
