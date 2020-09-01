ee73fc13dadd04f56facefc187f7efe1f62e08be
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-09-01 22:28:10 +0530

    Create pass_statement.py

---
+++ Classes/pass_statement.py
@@ -0,0 +1,14 @@
+#--------------------Empty class---------------------------
+
+class person:
+
+# If you compile this you will get an error
+"""
+Output:
+SyntaxError: unexpected EOF while parsing
+"""
+#------------------pass statement--------------------------
+
+class person:
+    pass
+# no errors
