248f34f711a40be84a30097dbfb298c4c5accf11
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-09-05 00:12:53 +0530

    Create Loop_through_a_file.py

---
+++ File handling/Reading a file/Loop_through_a_file.py
@@ -0,0 +1,11 @@
+f=open("text_file.txt","r")
+for item in f:
+    print(item,end='')
+
+"""
+Output-
+You have opened the file !
+You are using python .
+This file is for testing purpose .
+Ok the file is closing.
+"""
