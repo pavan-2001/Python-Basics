00d16a1a3d56c529a42f1bc5ccb3ba4ea3f377e5
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-09-04 23:41:42 +0530

    Create Democode.py

---
+++ File handling/Opening a file/Democode.py
@@ -0,0 +1,7 @@
+file=open("File_name.txt","r")         #opened in read mode
+file=open("File_name.txt","a")         #opened in append mode 
+file=open("File_name.txt","w")         #opened in write mode 
+file=open("File_name.txt","x")         #opened in create mode 
+file=open("File_name.txt","t")         #opened in test mode 
+file=open("File_name.txt","b")         #opened in binary mode 
+file=open("File_name.txt","rt")        #opened in read text mode 
