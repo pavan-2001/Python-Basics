71c916cc9e5ad4e741dc28a623a19ea2fe160aa0
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-09-05 00:14:09 +0530

    Create Closing_a_file.py

---
+++ File handling/Reading a file/Closing_a_file.py
@@ -0,0 +1,12 @@
+f=open("text_file.txt","r")
+for item in f:
+    print(item,end='')
+f.close()
+
+"""
+Output-
+You have opened the file !
+You are using python .
+This file is for testing purpose .
+Ok the file is closing.
+"""
